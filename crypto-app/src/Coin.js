d437e83a9052b5e794d8e2e7471549a3a001328a
Karsten Bjelde <fotball-karsten@hotmail.com>
2022-07-08 20:27:59 +0200

    Making the skeleton

---
+++ crypto-app/src/Coin.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Coin = () => {
+  return (
+    <div className="coin-container">
+      <div className="coin-row">
+        <div className="coin">
+          <img src={image} alt="crypto" />
+          <h1>{name}</h1>
+          <p className="coin-sympol">{symbol}</p>
+        </div>
+        <div className="coin-data">
+          <p className="coin-price">${price}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Coin;
